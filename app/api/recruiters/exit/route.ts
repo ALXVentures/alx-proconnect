ae91e3ed6b67438f47d6aaaf9945adcfc3ecefb9
chuqudee-sand <cokereafor@sandtech.com>
2026-09-09 12:48:37 +0100

    cleared V2.1

--- app/api/recruiters/exit/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { RECRUITER_COOKIE } from "@/lib/constants";
-
-export async function POST() {
-  const cookieStore = await cookies();
-  cookieStore.delete(RECRUITER_COOKIE);
-  return NextResponse.json({ ok: true });
-}
